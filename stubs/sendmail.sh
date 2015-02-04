ff69e56f2a0f6bf86683e6ba9c25a68c970e3e2b
Denis Ah-Kang <denis@w3.org>
2015-02-04 11:12:45 +0800

    Fix #65
    Send notification when an error occurred

--- stubs/sendmail.sh
+++ stubs/sendmail.sh
@@ -1,8 +1,29 @@
 #!/bin/bash
 
-DEST=$1
-URI=$2
+STATUS=$1
+DEST=$2
+URI=$3
+RESULT=$4
 
-mail -aFrom:webreq@w3.org -s "[W3C Publication] $URI had been published on http://www.w3.org/TR/" $DEST <<< "On `date -u`, $URI has been published by the W3C automated publication system.
+if [ $# -lt 3 ]; then
+    echo "`basename $0` [SUCCESS|ERROR] DESTINATION URI [RESULT]"
+    echo "`basename $0` SUCCESS foobar@w3.org http://www.w3.org/TR/YYYY/WD-shortname-YYYYMMDD"
+    exit 1
+fi
+
+if [ "SUCCESS" == "$STATUS" ]; then
+    SUBJECT="[W3C Publication] $URI had been published on http://www.w3.org/TR/"
+    BODY="On `date -u`, $URI has been published by the W3C automated publication system.
 
 If you believe there's an error, please contact webreq@w3.org."
+elif [ "ERROR" == "$STATUS" ]; then
+    SUBJECT="[W3C Publication] Publication of $URI failed"
+    BODY="On `date -u`, the request to publish $URI failed. See details below.
+
+$RESULT"
+fi
+
+if [ ! -z "$SUBJECT" -a ! -z "$BODY" ]; then
+    mail -aFrom:webreq@w3.org -s "$SUBJECT" $DEST <<< "$BODY"
+fi
+
