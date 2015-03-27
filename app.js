42059c535bda471209c306e25926a6b1c1558080
Jérémie Astori <jeremie@w3.org>
2015-03-27 16:39:01 -0400

    Fix wrong references to modules

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ var Uuid = require('node-uuid');
 
 var History = require('./lib/history');
 var Orchestrator = require('./lib/orchestrator');
-var SpecberusWrapper = require('../functions.js').SpecberusWrapper;
+var SpecberusWrapper = require('./functions.js').SpecberusWrapper;
 
 // Configuration file
 require('./config.js');
