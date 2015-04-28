11315fc40cf7b9b534c39f867bd410298f36fa50
tripu <tripu@tripu.info>
2015-04-28 16:30:04 +0900

    Two improvements to mail notifications.
    Subject line more compact, easier to parse quickly.
    
    For successful publications, include versions too.

--- app.js
+++ app.js
@@ -141,6 +141,9 @@ app.post('/api/request', function (req, res) {
       if (state.get('status') === 'error') {
         cmd += ' \'' + JSON.stringify(state, null, 2) + '\'';
       }
+      else {
+        cmd += ' \'Echidna ' + meta.version + '; Specberus ' + SpecberusWrapper.version + '\'';
+      }
 
       console.log('[' + state.get('status').toUpperCase() + '] ' + url);
       exec(cmd, function (err, _, stderr) { if (err) console.error(stderr); });
