1899c540924178e857769a10f72aabd6b5c9e2c5
jennyliang <jiaojiaomao220@163.com>
2022-11-15 12:11:33 +0800

    add test for verisoning

--- test/test.js
+++ test/test.js
@@ -372,6 +372,13 @@ describe('DocumentDownloader', () => {
 });
 
 describe('SpecberusWrapper', () => {
+  describe('get Specberus version()', () => {
+    const version = SpecberusWrapper.version();
+    it('should be a format of semantic versioning', () => {
+      expect(version).to.eventually.match(/\d+.\d+.\d+/);
+    });
+  });
+
   describe('validate(url)', () => {
     it('should be a function', () => {
       expect(SpecberusWrapper.validate).to.be.a('function');
